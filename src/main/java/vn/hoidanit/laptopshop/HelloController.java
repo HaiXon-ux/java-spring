9e5e97e5fe7f87fb9dc1e951a6d85d045a335cef
HaiXon-ux <danghaison321@gmail.com>
2025-07-12 09:09:34 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Your father!";
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only for user!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only for admin!";
     }
 
 }
